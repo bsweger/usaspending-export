68828fd391b8d8d967767c915b8b00973c974f7c
Becky <bsweger@gmail.com>
2017-11-09 03:38:18 +0000

    Merged in remove-data-folder (pull request #5)
    Load data from S3 instead of local folder
    
    Expand config file to include the S3 location of the input data.
    Remove old .json file from the data/ folder.
    Update data loaders to pull info from S3.
    
    Approved-by: Becky <bsweger@gmail.com>

--- load_usaspending.py
+++ load_usaspending.py
@@ -1,8 +1,14 @@
-import configparser
 import json
 import logging
+import os.path
+from urllib.parse import urlparse
 
+import boto
+from boto.exception import S3ResponseError
 from pymongo import MongoClient
+import smart_open
+
+from config.settings import settings
 
 
 def load_usaspending():
@@ -12,42 +18,46 @@ def load_usaspending():
 
     db = get_mongo_client()
 
-    # Insert Treasury Account Symbol (TAS) documents
-    tas = db.tas
-    with open('data/tas.json') as data:
-        return load_tas(tas, data)
-    # Insert awards documents
-    awards = db.awards
-    with open('data/awards.json') as data:
-        return load_awards(awards, data)
+    # For now, working under assumption that json data is somewhere on S3
+    key = get_s3_key(settings['s3']['tas'])
+    load_documents(db.tas, key)
+    key = get_s3_key(settings['s3']['awards'])
+    load_documents(db.awards, key)
 
 
-def load_tas(collection, file):
-    """Load usaspending account data."""
-    for row in file:
-        obj = json.loads(row)
-        tas_id = collection.insert_one(obj).inserted_id
-        logging.info('Inserted TAS {} as {}'.format(obj.get('label'), tas_id))
-
-
-def load_awards(collection, file):
-    """Load usaspending awards data."""
-    for row in file:
-        obj = json.loads(row)
-        award_id = collection.insert_one(obj).inserted_id
-        # omit logging until we've got a more formal system, to prevent
-        # writing millions of lines to the console
+def load_documents(collection, thefile):
+    """Load a file of json objects to the specified mongodb collection."""
+    with smart_open.smart_open(thefile) as data:
+        count = 0
+        for row in data:
+            collection.insert_one(json.loads(row)).inserted_id
+            count += 1
+        logging.info(
+            'Inserted {} documents into {}'.format(count, collection))
 
 
 def get_mongo_client():
     """Return a mongo database."""
-    config = configparser.ConfigParser()
-    config.read('config/config.ini')
-    mongo = config['mongodb']
+    mongo = settings['mongodb']
 
     client = MongoClient(mongo['host'], int(mongo['port']))
     db = client.usaspending
     return db
+
+
+def get_s3_key(s3url):
+    """Returns S3 key from URL."""
+    bucket, data = os.path.split(urlparse(s3url).path)
+    # using boto instead of boto3 to avail ourselves of some
+    # smart_open syntactic sugar when working with s3 data
+    try:
+        key = boto.connect_s3().get_bucket(bucket[1:]).get_key(data)
+    except S3ResponseError:
+        logging.error('Invalid S3 bucket: {}'.format(s3url))
+        raise
+    if key is None:
+        raise ValueError('Object {} not found on S3: {}'.format(data, s3url))
+    return key
 
 
 if __name__ == '__main__':
