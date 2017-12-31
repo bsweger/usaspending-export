591677ebe78bcf99503316c1448537d9a3bae73f
bsweger <bsweger@gmail.com>
2017-12-31 17:34:32 -0500

    Normalize and export awards data by federal account
    Add a mongo data pipeline to unwind the awards collection,
    creating a single record for each unique combination of award
    and federal account. Then export the results to .csv.

---
+++ flatten_usaspending.py
@@ -0,0 +1,50 @@
+import logging
+import subprocess
+
+from util.mongo import get_mongo_client
+
+from config.settings import settings
+
+
+def pipeline_awards(collection):
+    """Create a new collection of normalized awards."""
+    # TODO: more robust logging setup and execution
+    logging.basicConfig(level=logging.INFO)
+    db = get_mongo_client()
+
+    # define the mongo pipeline:
+    # 1. $project: exclude _id from the pipeline to prevent duplicate errors
+    #    on normalized data
+    # 2. $unwind: create a separate document for each account on an award
+    # 3. $out: the new collection to store the results
+    pipeline = [
+        {'$project': {'_id': 0}},
+        {'$unwind': '$accounting'},
+        {'$out': collection}
+    ]
+
+    # run the pipeline against the awards collection
+    logging.info('Normalizing usaspending awards...')
+    db.command(
+        'aggregate', 'awards', pipeline=pipeline
+    )
+
+
+def export_awards(collection, file):
+    """Export collection to .csv."""
+    db = settings['mongodb']['dbname']
+
+    args = ['mongoexport']
+    args.append(f'-d={db}')
+    args.append(f'-c={collection}')
+    args.append('--fieldFile=fields_awards.txt')
+    args.append('--type=csv')
+    args.append(f'-o={file}')
+
+    logging.info('Exporting normalized awards to csv...')
+    subprocess.run(args)
+
+
+if __name__ == '__main__':
+    pipeline_awards('awards_flat')
+    export_awards('awards_flat', 'usaspending_awards.csv')
